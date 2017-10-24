eee1c1dc3dcfe2055c4fcab54f700898a29b2c37
Cang Le <cang.b.le@gmail.com>
2017-10-24 13:08:31 -0700

    pokemon

--- src/components/RenderAllDecksComponent.js
+++ src/components/RenderAllDecksComponent.js
@@ -23,9 +23,9 @@ export default class RenderAllDecksComponent extends Component {
     };
   }
 
-  handle_deleteDecks = (event, id) => {
-    event.preventDefault();
-    this.props.delete_decks(id);
+  handle_deleteDecks = data => {
+    //console.log('clicked', data.target.id);
+    this.props.delete_decks(data.target.id);
   };
 
   handle_updateDecks = (event, id) => {
@@ -40,95 +40,90 @@ export default class RenderAllDecksComponent extends Component {
         <Grid columns={3} divided>
           <Grid.Row stretched>
             {this.props.userDecks.map((deck, i) => {
-              return deck.cards.map(pokemonObj => {
-                return (
-                  <Grid.Column>
-                    <Card.Group>
-                      <Card>
-                        <Icon floated="left" name="delete" />
-                        <Card.Content>
-                          <Image
-                            floated="right"
-                            size="mini"
-                            src={jenny}
-                            onClick={this.handle_deleteDecks}
-                          />
-                          <Card.Header>
-                            {deck.name}
-                          </Card.Header>
-                          <Card.Meta>User Name</Card.Meta>
-                          <List size="massive" horizontal>
-                            <Label size="small" image>
-                              <Image src={eevee} />
-                              eevee
-                            </Label>
-                            <Label size="small" image>
-                              <Image src={eevee} />
-                              eevee
-                            </Label>
-                            <Label size="small" image>
-                              <Image src={eevee} />
-                              eevee
-                            </Label>
-                            <Label size="small" image>
-                              <Image src={eevee} />
-                              eevee
-                            </Label>
-                            <Label size="small" image>
-                              <Image src={eevee} />
-                              eevee
-                            </Label>
-                            <Label size="small" image>
-                              <Image src={eevee} />
-                              eevee
-                            </Label>
-                          </List>
-                        </Card.Content>
-                        <Card.Content extra>
-                          <div className="ui two buttons">
-                            <Button basic color="green">
-                              Ready
-                            </Button>
-                            <Button
-                              basic
-                              color="red"
-                              onClick={this.handle_updateDecks}>
-                              Edit
-                            </Button>
-                          </div>
-                        </Card.Content>
-                        <Segment inverted>
-                          <Progress
-                            percent={parseInt(
-                              Math.round(
-                                deck.wins / (deck.wins + deck.losses) * 100
-                              )
-                            )}
-                            inverted
-                            progress
-                            success>
-                            WINS
-                          </Progress>
-                          <Progress
-                            percent={parseInt(
-                              Math.round(
-                                deck.losses / (deck.wins + deck.losses) * 100
-                              )
-                            )}
-                            inverted
-                            progress
-                            warning>
-                            LOSSES
-                          </Progress>
-                        </Segment>
-                      </Card>
-                    </Card.Group>
-                  </Grid.Column>
-                );
-              });
+              console.log('this deck is ', deck);
+              return (
+                <Grid.Column>
+                  <Card.Group>
+                    <Card>
+                      <Icon
+                        id={deck.id}
+                        floated="left"
+                        name="delete"
+                        onClick={this.handle_deleteDecks}
+                      />
+                      <Card.Content>
+                        <Image floated="right" size="mini" src={jenny} />
+                        <Card.Header>
+                          {deck.name}
+                        </Card.Header>
+                        <Card.Meta>User Name</Card.Meta>
+                        <List size="massive" horizontal>
+                          {deck.cards.map(character => {
+                            return (
+                              <Label size="small" image>
+                                <Image src={character.sprites.front_default} />
+                                {character.name}
+                              </Label>
+                            );
+                          })}
+                          {/* <Label size="small" image>
+                            <Image src={eevee} />
+                            eevee
+                          </Label>
+                          <Label size="small" image>
+                            <Image src={eevee} />
+                            eevee
+                          </Label>
+                          <Label size="small" image>
+                            <Image src={eevee} />
+                            eevee
+                          </Label> */}
+                        </List>
+                      </Card.Content>
+                      <Card.Content extra>
+                        <div className="ui two buttons">
+                          <Button basic color="green">
+                            Ready
+                          </Button>
+                          <Button
+                            basic
+                            color="red"
+                            onClick={this.handle_updateDecks}>
+                            Edit
+                          </Button>
+                        </div>
+                      </Card.Content>
+                      <Segment inverted>
+                        <Progress
+                          percent={parseInt(
+                            Math.round(
+                              deck.wins / (deck.wins + deck.losses) * 100
+                            )
+                          )}
+                          inverted
+                          progress
+                          success>
+                          WINS
+                        </Progress>
+                        <Progress
+                          percent={parseInt(
+                            Math.round(
+                              deck.losses / (deck.wins + deck.losses) * 100
+                            )
+                          )}
+                          inverted
+                          progress
+                          warning>
+                          LOSSES
+                        </Progress>
+                      </Segment>
+                    </Card>
+                  </Card.Group>
+                </Grid.Column>
+              );
             })}
           </Grid.Row>
-        </Grid>;
+        </Grid>
       </div>
     );
   }
