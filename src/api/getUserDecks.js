b165b52f8f16c2b0b80d404a276edf15dda2fa2a
Cang Le <cang.b.le@gmail.com>
2017-11-01 12:44:59 -0700

    pokewrapper

--- src/api/getUserDecks.js
+++ src/api/getUserDecks.js
@@ -1,6 +1,7 @@
 import recordToUserDeck from './utils/recordToUserDeck';
 
 export default function getUserDecks({ databaseId, token }) {
+  // fetch(`${env.API_BASE_URL}/users/${userId}/decks`)
   return fetch(`https://api.airtable.com/v0/${databaseId}/decks`, {
     headers: {
       Authorization: `Bearer ${token}`
