e0d61cd47e7c8f50436f4418ec12ed0a3738bd89
Cang Le <cang.b.le@gmail.com>
2017-11-29 15:07:05 -0800

    frontend

--- src/redux/thunks/getPokemonObjProcess.js
+++ src/redux/thunks/getPokemonObjProcess.js
@@ -5,6 +5,7 @@ export default function getPokemonObjProcess(pokemonId) {
   return (dispatch, getState) => {
     return getPokemonObj(pokemonId).then(result => {
       console.log('pokemonObj thunks------', result);
+      console.log('getDefaultPokemon thunks------', getDefaultPokemon());
       dispatch({
         type: 'FETCHED_POKEMON_OBJ',
         pokemonObj: result
