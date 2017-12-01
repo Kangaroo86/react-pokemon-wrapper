c59ed0962a504e772a83080421b2fd9c333c2729
Cang Le <cang.b.le@gmail.com>
2017-12-01 11:55:08 -0800

    battle page

--- src/components/BattlePageComponent.js
+++ src/components/BattlePageComponent.js
@@ -33,95 +33,13 @@ export default class RenderAllDecksComponent extends Component {
   render() {
     return (
       <div>
-        {/* <Grid columns="equal" divided padded>
+        <Grid columns="equal" padded>
           <Grid.Row textAlign="center">
             <Grid.Column>
-              <Segment color="blue">
-                <Image src={eevee} />
-                "eevee"
+              <Segment inverted color="red">
+                Player 1
               </Segment>
             </Grid.Column>
-            <Grid.Column>
-              <Segment color="black">
-                <Image src={eevee} />
-              </Segment>
-            </Grid.Column>
-            <Grid.Column>
-              <Segment color="black">
-                <Image src={eevee} />
-              </Segment>
-            </Grid.Column>
-            <Grid.Column>
-              <Segment color="black">
-                <Image src={eevee} />
-              </Segment>
-            </Grid.Column>
-            <Grid.Column>
-              <Segment color="black">
-                <Image src={eevee} />
-              </Segment>
-            </Grid.Column>
-          </Grid.Row>
-
-          <Grid.Row textAlign="center">
-            <Grid.Column>
-              <Segment color="black">
-                <Image src={eevee} />
-              </Segment>
-            </Grid.Column>
-            <Grid.Column>
-              <Segment color="black">
-                <Image src={eevee} />
-              </Segment>
-            </Grid.Column>
-            <Grid.Column>
-              <Segment color="black">
-                <Image src={eevee} />
-              </Segment>
-            </Grid.Column>
-            <Grid.Column>
-              <Segment color="black">
-                <Image src={eevee} />
-              </Segment>
-            </Grid.Column>
-            <Grid.Column>
-              <Segment color="black">
-                <Image src={eevee} />
-              </Segment>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid> */}
-
-        {/* <Grid celled>
-          <Grid.Row columns="equal">
-            <Grid.Column>
-              <Button basic color="green">
-                ATK
-              </Button>
-            </Grid.Column>
-            <Grid.Column>
-              <Button basic color="green">
-                ATK
-              </Button>
-            </Grid.Column>
-          </Grid.Row>
-
-          <Grid.Row columns="equal">
-            <Grid.Column>
-              <Button basic color="green">
-                ATK
-              </Button>
-            </Grid.Column>
-            <Grid.Column>
-              <Button basic color="green">
-                ATK
-              </Button>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid> */}
-
-        <Grid columns="equal" divided padded>
-          <Grid.Row textAlign="center">
             <Card.Group itemsPerRow={5}>
               <Card color="red" image={eevee} />
               <Card color="orange" image={eevee} />
@@ -129,9 +47,125 @@ export default class RenderAllDecksComponent extends Component {
               <Card color="olive" image={eevee} />
               <Card color="green" image={eevee} />
             </Card.Group>
+            <Grid.Column>
+              <Segment inverted color="red">
+                Player 1
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row>
+            {/* 1nd Player */}
+            <Grid.Column>
+              <Card.Group>
+                <Card>
+                  <Header as="h2">eevee</Header>
+                  <Grid columns="equal">
+                    <Grid.Row>
+                      <Grid.Column>
+                        <Label color="black">
+                          <Icon name="heartbeat" size="huge" />
+                          HP:
+                        </Label>
+                      </Grid.Column>
+                      <Grid.Column>
+                        <Progress
+                          active
+                          color="green"
+                          size="big"
+                          value="4"
+                          total="5"
+                          progress="percent"
+                        />
+                      </Grid.Column>
+                    </Grid.Row>
+                  </Grid>
+                  <Segment color="blue" textAlign="center">
+                    <Image src={eevee} centered size="small" />
+                  </Segment>
+                  <Segment inverted>
+                    <Menu compact icon="labeled">
+                      <Menu.Item name="bomb" onClick={''}>
+                        <Icon name="bomb" />
+                        Attack
+                      </Menu.Item>
+                      <Menu.Item
+                        name="lightning"
+                        onClick={this.handleItemClick}>
+                        <Icon name="lightning" />
+                        Attack
+                      </Menu.Item>
+                      <Menu.Item
+                        name="video play"
+                        onClick={this.handleItemClick}>
+                        <Icon name="shield" />
+                        Defend
+                      </Menu.Item>
+                    </Menu>
+                  </Segment>
+                </Card>
+              </Card.Group>
+            </Grid.Column>
+
+            {/* 2nd Player */}
+            <Grid.Column>
+              <Card.Group>
+                <Card>
+                  <Header as="h2">eevee</Header>
+                  <Grid columns="equal">
+                    <Grid.Row>
+                      <Grid.Column>
+                        <Label color="black">
+                          <Icon name="heartbeat" size="huge" />
+                          HP:
+                        </Label>
+                      </Grid.Column>
+                      <Grid.Column>
+                        <Progress
+                          active
+                          color="green"
+                          size="big"
+                          value="4"
+                          total="5"
+                          progress="percent"
+                        />
+                      </Grid.Column>
+                    </Grid.Row>
+                  </Grid>
+                  <Segment color="blue" textAlign="center">
+                    <Image src={eevee} centered size="small" />
+                  </Segment>
+                  <Segment inverted>
+                    <Menu compact icon="labeled">
+                      <Menu.Item name="bomb" onClick={''}>
+                        <Icon name="bomb" />
+                        Attack
+                      </Menu.Item>
+                      <Menu.Item
+                        name="lightning"
+                        onClick={this.handleItemClick}>
+                        <Icon name="lightning" />
+                        Attack
+                      </Menu.Item>
+                      <Menu.Item
+                        name="video play"
+                        onClick={this.handleItemClick}>
+                        <Icon name="shield" />
+                        Defend
+                      </Menu.Item>
+                    </Menu>
+                  </Segment>
+                </Card>
+              </Card.Group>
+            </Grid.Column>
           </Grid.Row>
 
           <Grid.Row textAlign="center">
+            <Grid.Column>
+              <Segment inverted color="orange">
+                Player 2
+              </Segment>
+            </Grid.Column>
             <Card.Group itemsPerRow={5}>
               <Card color="red" image={eevee} />
               <Card color="orange" image={eevee} />
@@ -139,10 +173,15 @@ export default class RenderAllDecksComponent extends Component {
               <Card color="olive" image={eevee} />
               <Card color="green" image={eevee} />
             </Card.Group>
+            <Grid.Column>
+              <Segment inverted color="orange">
+                Player 2
+              </Segment>
+            </Grid.Column>
           </Grid.Row>
         </Grid>
 
-        <Grid columns="equal" padded>
+        {/* <Grid columns="equal" padded>
           <Grid.Row>
             <Grid.Column>
               <Card.Group>
@@ -197,7 +236,7 @@ export default class RenderAllDecksComponent extends Component {
               </Card.Group>
             </Grid.Column>
           </Grid.Row>
-        </Grid>
+        </Grid> */}
       </div>
     );
   }
