d329330fd83496d7ce2c41820ce89ecea366daba
Cang Le <cang.b.le@gmail.com>
2018-02-20 19:58:12 -0800

    added aws links to .env file

--- src/socket.io/socketManager.js
+++ src/socket.io/socketManager.js
@@ -3,6 +3,7 @@ import env from '../env';
 const socketUrl = `${env.API_BASE_URL}`; //socket-io
 export const socket = io(socketUrl); //exported to battlePageContainer
 
+//console.log('url-----------', socketUrl);
 //initalized socket & created room
 socket.on('connect', () => {
   console.log('Socket initalized: ', socket.id);
